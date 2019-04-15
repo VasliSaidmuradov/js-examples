a2efa5f6830bff669316c91f25d363b61e9faffc
Vasli Saidmuradov <vasli.said@gmail.com>
2019-04-15 11:22:22 +0600

    Add: #4 learn javascript.ru folder for do examples

---
+++ #4 learn.javascript.ru/alert.js
@@ -0,0 +1,18 @@
+//alert("Hi!");
+var admin, name = "vasli";
+admin = name;
+console.log("admin: " + admin);
+console.log("name: " + name); 
+
+var ourPlanetName = "Earth", userName = "Vasya";
+console.log("Our Planet: " + ourPlanetName);
+console.log("User name: " + userName);
+
+var num = 111, str = "Some string", bool = true, undef, nul = null, obj = { name: "Vasli"};
+console.log(num + " is- " + typeof num + ";", str + " is- " + typeof str + ";", bool + " is- " + typeof bool + ";", undef + " is- " + typeof undef + ";", nul + " is- " + typeof nul + ";", obj + " is- " + typeof obj);
+
+var myNumberType = typeof (1+"place");
+console.log(myNumberType);
+
+var myStr = "My " + "string";
+console.log("my " + 1);
